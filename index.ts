dc57ff5d73d96cfdeb2264892ab8fdb9524f573c
José Gerónimo <50242795+jose-geronimo@users.noreply.github.com>
2022-01-15 22:11:49 -0600

    reparación

--- index.ts
+++ index.ts
@@ -1,11 +1,6 @@
 import Server from "./classes/server";
 import userRoutes from "./routes/usuario";
-import activityRoutes from "./routes/actividades";
 import binnacleRoutes from "./routes/bitacora";
-import hiveRoutes from "./routes/colmena";
-import yieldRoutes from "./routes/cosecha";
-import outgoingsRoutes from "./routes/gastos"
-import observationRoutes from "./routes/observacion"
 import mongoose, { ConnectOptions } from "mongoose";
 import bodyParser from "body-parser";
 
@@ -28,12 +23,8 @@ server.app.use( bodyParser.json() );
 
 //RUTAS DE LA APLICACIÓN
 server.app.use('/user', userRoutes);
-server.app.use('/activities', activityRoutes);
 server.app.use('/binnacle', binnacleRoutes);
-server.app.use('/hive', hiveRoutes);
-server.app.use('/yield', yieldRoutes);
-server.app.use('/outgoings', outgoingsRoutes);
-server.app.use('/observation', observationRoutes);
+
 
 //CONECTAR A BD
 const uri = "mongodb+srv://SANDY:hU9JqTTMHwTZ6kKT@beeappbackendbd.alzsj.mongodb.net/beeAppBackendBD?retryWrites=true&w=majority";
